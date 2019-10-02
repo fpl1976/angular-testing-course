b58e694c9a5e409f7e7d09cb80adf33b9a6c9b58
Felix Prados <f.prados@safened.com>
2019-10-02 17:55:57 +0200

    Update server port

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,7 @@
-
-
 import * as express from 'express';
-import {Application} from "express";
-import {getAllCourses, getCourseById} from "./get-courses.route";
-import {searchLessons} from "./search-lessons.route";
+import {Application} from 'express';
+import {getAllCourses, getCourseById} from './get-courses.route';
+import {searchLessons} from './search-lessons.route';
 import {saveCourse} from './save-course.route';
 
 const bodyParser = require('body-parser');
@@ -22,8 +20,8 @@ app.route('/api/lessons').get(searchLessons);
 app.route('/api/courses/:id').put(saveCourse);
 
 
-const httpServer = app.listen(9000, () => {
-    console.log("HTTP REST API Server running at http://localhost:" + httpServer.address().port);
+const httpServer = app.listen(9001, () => {
+    console.log('HTTP REST API Server running at http://localhost:' + httpServer.address().port);
 });
 
 
